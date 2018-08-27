f3b466e3d8037ee03f2710839d688b19b1586669
KGY <p82468@paran.com>
2018-08-27 18:19:14 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,6 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-REQUIRED_PACKAGES = ['numpy', 'Pillow>=1.0', 'Flask', 'tensorflow', 'six', 'matplotlib']
-
-setup(
-    name='webrtc_object_detection',
-    version='0.1',
-    install_requires=REQUIRED_PACKAGES,
-    include_package_data=True,
-    packages=[p for p in find_packages()],
-    description='Tensorflow Object Detection with WebRTC',
-)
-
 '''Download the Object Dectection directory'''
 import six.moves.urllib as urllib
 from zipfile import ZipFile

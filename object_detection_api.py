ae8b85af3ae9b6d7f24a09d2f926a12d2452347b
KGY <p82468@paran.com>
2018-08-27 18:20:39 +0900

    Update object_detection_api.py

--- object_detection_api.py
+++ object_detection_api.py
@@ -7,7 +7,7 @@ import tarfile
 import tensorflow as tf
 import json
 
-if tf.__version__ != '1.4.0':
+if tf.__version__ != '1.3.0':
   raise ImportError('Please upgrade your tensorflow installation to v1.4.0!')
 
 # ENV SETUP  ### CWH: remove matplot display and manually add paths to references
